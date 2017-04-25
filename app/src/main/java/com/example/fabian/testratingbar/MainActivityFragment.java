5f79786e3095fbbf338016719412b38085c009f1
Fabian <20464654j@iespoblenou.org>
2017-04-25 10:24:22 +0200

    Añadimos un OnClickListener al boton para que nos muestre el valor del RatingBar en un Toast

--- app/src/main/java/com/example/fabian/testratingbar/MainActivityFragment.java
+++ app/src/main/java/com/example/fabian/testratingbar/MainActivityFragment.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private Button btSubmit;
+    private RatingBar ratingBar;
 
     public MainActivityFragment() {
     }
@@ -18,6 +24,20 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        btSubmit = (Button) view.findViewById( R.id.submint_button );
+        ratingBar = (RatingBar) view.findViewById( R.id.ratingBar );
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String rating = String.valueOf( ratingBar.getRating() );
+
+                Toast.makeText( getContext(), rating, Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         return view;
     }
